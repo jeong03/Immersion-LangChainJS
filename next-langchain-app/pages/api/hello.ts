268d74019cd56727e7efb6b5d11a995d26858ee6
jeong03 <hjeong7367@gmail.com>
2024-08-22 14:28:18 +0900

    commit

--- next-langchain-app/pages/api/hello.ts
+++ next-langchain-app/pages/api/hello.ts
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// 호출주쇠: http://localhost:3000/api/hello
+// 라우팅 주소는 /api 폴더 아래 물리적 폴더명과 파일명으로 라우팅 주소가 설정됨
+
+// NextApiResult 타입은 웹브라우저에서 서버로 전달되는 각종 정보를 추출하는 HTTPRequest 객체 = req
+// NextApiResponse 타입은 서버에서 웹브라우저로 전달하는 응답처리를 위한 HttpResponse 객체 = res
 import type { NextApiRequest, NextApiResponse } from "next";
 
+// 서버에서 웹브라우저로 반환하는 처리결과 데이터 타입
 type Data = {
   name: string;
 };
